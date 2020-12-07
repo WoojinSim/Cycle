f57c29a8c6f7b5e294b245fce5e17be675b14932
WoojinSim <ssqwe2@gmail.com>
2020-12-08 02:03:45 +0900

    정보 명령어 내용 추가 및 리스너 수정

--- src/woojinsim/Cycle/Commands/Info.java
+++ src/woojinsim/Cycle/Commands/Info.java
@@ -2,14 +2,16 @@ package woojinsim.Cycle.Commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class Info {
-	public static void run(TextChannel Channel, String[] args) {
-		Channel.sendTyping().queue();
+	public static void run(GuildMessageReceivedEvent event, String[] args) {
 		EmbedBuilder info = new EmbedBuilder();
 		info.setColor(0x578BD2);
 		info.setTitle("🔹 안녕하세요 저는 종합 게임 봇 사이클입니다");
-		info.setDescription("저는 2020-12-07 부터 Woojin Sim 님에 의해 개발되었습니다.");
-		Channel.sendMessage(info.build()).queue();
+		info.setDescription(" ▫ 저는 2020-12-07 부터 Woojin Sim 님에 의해 개발되었습니다.\n"
+				+ " ▫ 저는 오픈소스로 개발되고 있습니다. 저의 [깃허브](https://github.com/WoojinSim/Cycle)를 참조해주세요.");
+		info.setFooter(event.getMessage().getAuthor().getAsTag() + " 님께서 실행함", event.getMessage().getAuthor().getAvatarUrl());
+		event.getChannel().sendMessage(info.build()).queue();
 	}
 }
--- src/woojinsim/Cycle/Listener.java
+++ src/woojinsim/Cycle/Listener.java
@@ -15,7 +15,7 @@ public class Listener extends ListenerAdapter{
 			command = args[0].toLowerCase().substring(1);
 			
 			if (command.equals("정보") || command.equals("info")) {
-				Info.run(event.getChannel(), args);
+				Info.run(event, args);
 				
 			} else if (command.equals("청소") || command.equals("삭제") || command.equals("delete") || command.equals("del")) {
 				Clear.run(event, args);
